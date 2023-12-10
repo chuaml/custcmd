86c360d84847e9d4fb16d863568715123d7c868a
chuaml <chuaml@outlook.com>
2023-12-10 18:57:41 +0800

    zip now take 1st argument as path to zip

--- zip.bat
+++ zip.bat
@@ -1,3 +1,25 @@
 @echo off
-for %%I in (.) do set currentDirName=%%~nxI
-7z a -tzip -mx1 "%currentDirName%.zip" %*
+@REM %~1 = path = 1st given arguement with dobule quotes removed
+if "%~1"=="" (
+    echo specify a path to zip files
+    echo example:
+    echo     zip "[ dir | filename ]"
+) else (
+    @REM bug fix, to allow use !varName_123! where variable is the latest set value
+    setlocal enabledelayedexpansion
+
+    @REM set current dir name
+    for %%I in ("%~1") do set currentDirName=%%~nxI
+
+    if not exist "%~1" (
+        echo "directory not found:  %~1"
+    ) else (
+        if exist "%~1/*" (
+            @REM zip all files in %~1
+            7z a -tzip -mx1 "!currentDirName!.zip" "%~1/*"
+        ) else (
+            @REM zip a file; %~1 is a file , not dir
+            7z a -tzip -mx1 "!currentDirName!.zip" "%~1"
+        )
+    )
+)
