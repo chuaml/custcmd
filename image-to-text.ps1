760a0720d80d66a6790e4b26e514edfc9ff498fa
chuaml <chuaml@outlook.com>
2025-09-08 17:17:43 +0800

    add comment and improve usage

--- image-to-text.ps1
+++ image-to-text.ps1
@@ -1,17 +1,55 @@
-# this script scan all picture in current dir, output to a text file
-
-# require ImageMagick
-# require Tesseract
-# ----------------------------------------------
-
-
+<#
+.SYNOPSIS
+    this script scan all picture in a dir, output to text files
+.DESCRIPTION
+    dependencies:
+      require ImageMagick
+      require Tesseract
+.LINK
+    references: 
+    * ImageMagick
+        - param arguments:  https://imagemagick.org/script/command-line-options.php
+    * Tesseract
+        - improve quality:  https://tesseract-ocr.github.io/tessdoc/ImproveQuality.html
+        - trained language data:  https://github.com/tesseract-ocr/tessdata_best
+.EXAMPLE
+    image-to-text . outputDir -language eng -resizePercentage 50% -combineOutputFile y
+.EXAMPLE
+    image-to-text . outputDir -language eng+equ -resizePercentage 25% -combineOutputFile n
+#>
 param(
+    [switch]$help,
+
     [Parameter(Mandatory, HelpMessage = "directory to process; e.g.  .")]
+    [ArgumentCompleter({ "." })]
     [string]$dir,
-    [Parameter(Mandatory, HelpMessage = "output dir; e.g. ./temp/")]
-    [string]$outputDir
+
+    [Alias("o")]
+    [Parameter(Mandatory, HelpMessage = "output dir; e.g. ./outputDir/")]
+    [ArgumentCompleter({ "outputDir" })]
+    [string]$outputDir,
+    
+    [Parameter(Mandatory, HelpMessage = "language of tesseract .traineddata (e.g.: eng)")]
+    [ArgumentCompleter({ "eng", "eng+equ" })]
+    [string]$language = "eng",
+
+    [Parameter(Mandatory, HelpMessage = "resize preprocessed images, too big no good (e.g. 50%)")]
+    [ArgumentCompleter({ "50%", "25%" })]
+    [string]$resizePercentage="50%",
+
+    [Parameter(Mandatory, HelpMessage = "yes or no?")]
+    [ValidateSet("y", "n")]
+    [ArgumentCompleter({ "y", "n" })]
+    [char]$combineOutputFile
 )
 Set-StrictMode -Version Latest
+
+if ($help) {
+    # display help
+    Get-Help $MyInvocation.MyCommand.Name
+    exit 0;
+}
+
 
 # 0. show commands when run
 Set-PSDebug -Trace 1
@@ -21,8 +59,8 @@ if (-not (Test-Path -Path $outputDir)) {
 }
 
 # 1. convert and normalize pictures to black&white high quality text image
-## resize to half (too big no good)
-Get-ChildItem -Path "$dir/*" -File -Include "*.png", "*.jpg", "*.jpeg" | ForEach-Object { magick $_.FullName -colorspace gray -sharpen 0x1 -normalize -threshold 50% -resize 50% -sharpen 0x1 -bordercolor white -border 16 ("$outputDir/" + $_.Name) } 
+## and resize image (too big no good)
+Get-ChildItem -Path "$dir/*" -File -Include "*.png", "*.jpg", "*.jpeg" | ForEach-Object { magick $_.FullName -colorspace gray -sharpen 0x1 -normalize -threshold 50% -resize $resizePercentage -sharpen 0x1 -bordercolor white -border 16 ("$outputDir/" + $_.Name) } 
 
 
 
@@ -32,12 +70,13 @@ Get-ChildItem -Path "$dir/*" -File -Include "*.png", "*.jpg", "*.jpeg" | ForEach
 $OutputEncoding = [Console]::InputEncoding = [Console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
 ## exec
-Get-ChildItem -Path "$outputDir/*" -File -Include "*.png", "*.jpg", "*.jpeg"  | ForEach-Object { tesseract $_.FullName stdout --oem 1 --psm 6 -c preserve_interword_spaces=1 -l eng } > "$outputDir/output.txt"
-
-# # for separate .txt files
-# cd $outputDir
-# Get-ChildItem -Path "./*" -File -Include "*.png", "*.jpg", "*.jpeg"  | ForEach-Object { tesseract $_.FullName $_.BaseName --oem 1 --psm 6 -c preserve_interword_spaces=1 -l eng }
-# Get-Content *.txt | Set-Content output.txt
+if ($combineOutputFile -eq "y") {
+    Get-ChildItem -Path "$outputDir/*" -File -Include "*.png", "*.jpg", "*.jpeg"  | ForEach-Object { tesseract $_.FullName stdout --oem 1 --psm 6 -c preserve_interword_spaces=1 -l $language } > "$outputDir/output.txt"
+}
+else {
+    ## for separate .txt files
+    Get-ChildItem -Path "$outputDir/*" -File -Include "*.png", "*.jpg", "*.jpeg"  | ForEach-Object { tesseract $_.FullName ("$outputDir/" + $_.BaseName) --oem 1 --psm 6 -c preserve_interword_spaces=1 -l $language }
+}
 
 # reference: (Tesseract)
 # https://tesseract-ocr.github.io/tessdoc/ImproveQuality.html
